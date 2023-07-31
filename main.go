cf13283158cc71575958013ff91b0aac537103f0
Tomáš Kukrál <tom+noreply@6shore.net>
2023-07-31 10:46:03 +0200

    load certificate automatically
    This adds posibility to change certificate and let env-injector to
    reload it. It can be optimised by rereading certificate file only on
    change/signal but it'd complicate code a lot so I decided to go with
    this easy option with small perfomance penalty.

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	
+
 	"github.com/golang/glog"
 )
 
@@ -22,42 +22,46 @@ func main() {
 	flag.StringVar(&parameters.keyFile, "tlsKeyFile", "/etc/webhook/certs/key.pem", "File containing the x509 private key to --tlsCertFile.")
 	flag.StringVar(&parameters.envCfgFile, "envCfgFile", "/etc/webhook/config/envconfig.yaml", "File containing the mutation configuration.")
 	flag.Parse()
-	
+
 	envConfig, err := loadConfig(parameters.envCfgFile)
 	if err != nil {
 		glog.Errorf("Error loading configuration: %v", err)
 	}
-	
-	pair, err := tls.LoadX509KeyPair(parameters.certFile, parameters.keyFile)
-	if err != nil {
-		glog.Errorf("Error loading key pair: %v", err)
-	}
-	
-	whsvr := &WebhookServer {
-		envConfig: 	envConfig,
-		server:     &http.Server {
-			Addr:        fmt.Sprintf(":%v", parameters.port),
-			TLSConfig:   &tls.Config{Certificates: []tls.Certificate{pair}},
+
+	whsvr := &WebhookServer{
+		envConfig: envConfig,
+		server: &http.Server{
+			Addr: fmt.Sprintf(":%v", parameters.port),
+			TLSConfig: &tls.Config{
+				GetCertificate: func(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+					pair, err := tls.LoadX509KeyPair(parameters.certFile, parameters.keyFile)
+					if err != nil {
+						return nil, fmt.Errorf("Error loading key pair: %w", err)
+					}
+
+					return &pair, nil
+				},
+			},
 		},
 	}
-	
+
 	// define http server and server handler
 	mux := http.NewServeMux()
 	mux.HandleFunc("/mutate", whsvr.serve)
 	whsvr.server.Handler = mux
-	
+
 	// start webhook server in new rountine
 	go func() {
 		if err := whsvr.server.ListenAndServeTLS("", ""); err != nil {
 			glog.Errorf("Filed to listen and serve env-injector-webhook server: %v", err)
 		}
 	}()
-	
+
 	// listening OS shutdown signal
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 	<-signalChan
-	
+
 	glog.Infof("Got OS shutdown signal, shutting down env-injector-webhook server...")
 	whsvr.server.Shutdown(context.Background())
 }
